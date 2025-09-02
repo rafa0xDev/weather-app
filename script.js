c2dfe1f37b4f065b7ab46908c720bb1b3a7269f2
RohanAlief <rafarafa92653@gmail.com>
2025-09-02 21:17:48 +0700

    feat : using setTimeout for loading

--- script.js
+++ script.js
@@ -13,24 +13,33 @@ form.addEventListener('submit', (e) => {
 
 //Simulasikan data cuaca:
 function getWeather(location) {
-    //Ambil teks dari input:
     if(location === '') {
-        alert('Please enter a location');
-        return;
-    }
+            alert('Please enter a location');
+            weatherResult.classList.remove('show');
+            return;
+        }
+
+    //Ambil teks dari input:
+    weatherResult.classList.add('show');
+
+    document.body.style.backgroundColor = "#1a202c"; // atau warna default
+    weatherResult.innerHTML = 'Mendapatkan data cuaca...';
     weatherResult.classList.add('show');
     
-    switch(location.toLowerCase()) {
-        case 'jakarta':
-            weatherResult.innerHTML = 'Cuaca di Jakarta: Cerah, 30°C';
-            break;
-        case 'tokyo':
-            weatherResult.innerHTML = 'Cuaca di Tokyo: Hujan, 19°C';
-            break;
-        case 'london':
-            weatherResult.innerHTML = 'Cuaca di London: Berawan, 22°C';
-            break;
-        default:
-            weatherResult.innerHTML = 'Lokasi tidak ditemukan';
-    }
+    setTimeout(() => {
+        
+        switch(location.toLowerCase()) {
+            case 'jakarta':
+                weatherResult.innerHTML = 'Cuaca di Jakarta: Cerah, 30°C';
+                break;
+            case 'tokyo':
+                weatherResult.innerHTML = 'Cuaca di Tokyo: Hujan, 19°C';
+                break;
+            case 'london':
+                weatherResult.innerHTML = 'Cuaca di London: Berawan, 22°C';
+                break;
+            default:
+                weatherResult.innerHTML = 'Lokasi tidak ditemukan';
+        }
+    }, 1000);
 }
