65e4a438774b7acce47b593c8212bbba4a220e6e
rafadev <rafarafa92653@gmail.com>
2026-03-24 23:34:12 +0700

    feat: connect air conditions to API data

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 
 async function getWeather(city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
-    console.log(response)
-    console.log("------------------------------")
-    const data = await response.json()
-    console.log(data)
-    console.log(data.name)
-    const temp = data.main.temp//300.92
-    const celcius = temp - 273.15
-    console.log(celcius)//27.77
-    console.log(data.weather[0].main)//rain
+    const data = await response.json();
+
+    const cityName = document.querySelector('.city-name');
+    const weatherTemp = document.querySelector('.weather-temp');
+    const realfeelval = document.getElementById('RealFeel-value');
+    const windval = document.getElementById('Wind-value');
+
+    cityName.textContent = data.name;
+    weatherTemp.textContent = `${Math.round(data.main.temp - 273.15)}°`;
+    realfeelval.textContent = `${Math.round(data.main.feels_like - 273.15)}°`;
+    windval.textContent = `${data.wind.speed} km/h`;
+    
+
 }
 
-getWeather('Kudus') //300.92
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        const city = searchInput.value;
+        getWeather(city);
+    } else {
+        return;
+    }
+});
+
+
+
+
